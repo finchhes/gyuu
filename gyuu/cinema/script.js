5aa25756f16743d8e73b0213fc1ff2591fe82969
louvre <reniick99@gmail.com>
2025-03-30 18:54:51 -0400

    created cinema page

---
+++ gyuu/cinema/script.js
@@ -0,0 +1,67 @@
+
+var iso = new Isotope(".grid", {
+  itemSelector: ".item",
+  getSortData: {
+    name: ".name",
+    date: ".date parseInt",
+    reverse: ".date parseInt",
+  },
+  sortAscending: {
+    date: false,
+    reverse: true,
+  },
+  layoutMode: "fitRows",
+});
+
+
+// bind sort button click
+var sortByGroup = document.querySelector(".sort-by-button");
+sortByGroup.addEventListener("click", function (event) {
+  if (!matchesSelector(event.target, ".button")) return;
+  var sortValue = event.target.getAttribute("data-sort-value");
+  iso.arrange({ sortBy: sortValue });
+});
+
+// flatten object by concatting values
+function concatValues(obj) {
+  var value = "";
+  for (var prop in obj) {
+    value += obj[prop];
+  }
+  return value;
+}
+
+// store filter for each group
+var filters = {};
+var filtersElem = document.querySelector(".filters");
+filtersElem.addEventListener("click", function (event) {
+  var isButton = event.target.classList.contains("button");
+  if (!isButton) return;
+
+  var buttonGroup = fizzyUIUtils.getParent(event.target, ".button-group");
+  var filterGroup = buttonGroup.getAttribute("data-filter-group");
+  // set filter for group
+  filters[filterGroup] = event.target.getAttribute("data-filter");
+  // combine filters
+  var filterValue = concatValues(filters);
+  // set filter for Isotope
+  iso.arrange({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll(".button-group");
+for (var i = 0; i < buttonGroups.length; i++) {
+  var buttonGroup = buttonGroups[i];
+  var onButtonGroupClick = getOnButtonGroupClick(buttonGroup);
+  buttonGroup.addEventListener("click", onButtonGroupClick);
+}
+
+function getOnButtonGroupClick(buttonGroup) {
+  return function (event) {
+    // check for only button clicks
+    var isButton = event.target.classList.contains("button");
+    if (!isButton) return;
+    buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
+    event.target.classList.add("is-checked");
+  };
+}
